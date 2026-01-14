120d0bfca0e2c9dfe22136468068ec9e9ac80a92
André Araújo <andre.araujo.tech@gmail.com>
2026-01-13 21:41:59 -0300

    Adicionando exercício 1 - concatenando dados

---
+++ resolucoes_andre/resolucoes_andre/01_concatenando_dados.py
@@ -0,0 +1,9 @@
+# Exercício 1 - Concatenando Dados
+# Autor: André Araújo
+
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+resultado = dado1 + " " + dado2
+
+print("Resultado:", resultado)
